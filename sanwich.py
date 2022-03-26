59cbeab5d725df75942fdd01ced7988bee0760c4
LEE-I-HUA <93990513+LEE-I-HUA@users.noreply.github.com>
2022-03-26 22:45:01 +0800

    Add files via upload

---
+++ sanwich.py
@@ -0,0 +1,9 @@
+def main():
+    sandwich=[ "tuna", "beef", "strawberry", "grilled cheese", "turkey"]
+    order = []
+    for value in sandwich:
+        order.append(value)
+    for value in order:
+        print("yor", value, "sandwich order is already completed")
+main()
+input()
